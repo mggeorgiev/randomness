baa2babdcdfb9e06759258acbacd97ba11264567
Martin Georgiev <mggeorgiev@gmail.com>
2021-12-19 23:14:44 +0100

    initial version of coin toss

--- coins/GenerateRandonCoinSequence.cs
+++ coins/GenerateRandonCoinSequence.cs
@@ -29,12 +29,14 @@ namespace coins
         [OpenApiSecurity("function_key", SecuritySchemeType.ApiKey, Name = "code", In = OpenApiSecurityLocationType.Query)]
         [OpenApiParameter(name: "throws", In = ParameterLocation.Query, Required = true, Type = typeof(string), Description = "The **Throw** parameter.")]
         [OpenApiParameter(name: "groups", In = ParameterLocation.Query, Required = true, Type = typeof(string), Description = "The **Groups** parameter. How many sets of throws to be returned.")]
-        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The OK response")]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(RootObject), Description = "The OK response")]
         [OpenApiResponseWithBody(statusCode: HttpStatusCode.BadRequest, contentType: "text/plain", bodyType: typeof(string), Description = "The 400 response")]
         public async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            //var datas = new ResultData();
 
             string throws = req.Query["throws"];
             string groups = req.Query["groups"];
@@ -51,17 +53,17 @@ namespace coins
             if (tossToDeliver > 100)
                 return new BadRequestObjectResult("Cannot return more than 100 coin throws.");
 
-            int groupsTodeliver = 8;
+            int groupsToDeliver = 8;
             if (groups != null)
-                groupsTodeliver = Convert.ToInt32(throws);
-            if (groupsTodeliver > 100 && groupsTodeliver < 1)
+                groupsToDeliver = Convert.ToInt32(throws);
+            if (groupsToDeliver > 100 && groupsToDeliver < 1)
                 return new BadRequestObjectResult("Cannot return less then 1 and more than 100 groups of throws.");
 
-            var listOfThrows = new List<string>();
             var listofListOfThrows = new List<List<string>>();
 
-            for (int n = 0; n < groupsTodeliver; n++)
+            for (int n = 0; n < groupsToDeliver; n++)
             { 
+                var listOfThrows = new List<string>();
                 for (int i = 0; i < tossToDeliver; i++)
                 {
                     Random r = new Random();
@@ -71,18 +73,31 @@ namespace coins
                     if (tossCoin > 0.5) result = "H";
 
                     listOfThrows.Add(result);
+                    //datas.Add(result);
                 }
                 listofListOfThrows.Add(listOfThrows);
+                //listOfThrows.Clear();
             }
 
             string defaultResponseMessage = string.IsNullOrEmpty(throws)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
                 : $"Hello, toy requested a sequence of {throws}. This HTTP triggered function executed successfully.";
 
-            string responseMessage = JsonConvert.SerializeObject(listofListOfThrows, Formatting.Indented);
+            var rb = new RootObject();
+            rb.TossesPerGroup = tossToDeliver;
+            rb.NumberOfGroups = groupsToDeliver;
+            rb.Data = listofListOfThrows;
+            string responseMessage = JsonConvert.SerializeObject(rb, Formatting.Indented);
 
             return new OkObjectResult(responseMessage);
         }
+    }
+
+    public class RootObject
+    {
+        public int NumberOfGroups { get; set; }
+        public int TossesPerGroup { get; set; }
+        public List<List<string>> Data { get; set; }
     }
 }
 
