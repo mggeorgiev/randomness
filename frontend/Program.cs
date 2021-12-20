9b795c90123267ca93081987603c7d50091e31c8
Martin Georgiev <Martin.Georgiev@fujitsuts.onmicrosoft.com>
2021-12-20 14:32:32 +0100

    Add settings to frontend

--- frontend/Program.cs
+++ frontend/Program.cs
@@ -6,6 +6,19 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+var http = new HttpClient()
+{
+    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+};
+
+builder.Services.AddScoped(sp => http);
+
+using var response = await http.GetAsync("coins.json");
+using var stream = await response.Content.ReadAsStreamAsync();
+
+builder.Configuration.AddJsonStream(stream);
 
 await builder.Build().RunAsync();
