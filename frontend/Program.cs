3fac36b83657569b7c37740d576864cda94ea507
Martin Georgiev <mggeorgiev@gmail.com>
2021-12-21 10:39:46 +0100

    add functions configuration

--- frontend/Program.cs
+++ frontend/Program.cs
@@ -16,14 +16,9 @@ var http = new HttpClient()
 
 builder.Services.AddScoped(sp => http);
 
-using var response = await http.GetAsync("coins.json");
+using var response = await http.GetAsync("functions_config.json");
 using var stream = await response.Content.ReadAsStreamAsync();
 
 builder.Configuration.AddJsonStream(stream);
-
-using var response_points = await http.GetAsync("points.json");
-using var stream_points = await response.Content.ReadAsStreamAsync();
-
-builder.Configuration.AddJsonStream(stream_points);
 
 await builder.Build().RunAsync();
