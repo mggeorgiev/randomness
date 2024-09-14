23e643fabf14a3bf21e07b11ef0a27731873c6df
Martin Georgiev <mggeorgiev@gmail.com>
2024-09-14 16:20:23 +0200

    add initial version of the isolated function

---
+++ afn-random-functions/healthcheck.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace afn_random_functions
+{
+    public class Healthcheck
+    {
+        private readonly ILogger<Healthcheck> _logger;
+
+        public Healthcheck(ILogger<Healthcheck> log)
+        {
+            _logger = log;
+        }
+
+        [FunctionName("healthcheck")]
+        [OpenApiOperation(operationId: "Run", tags: new[] { "name" })]
+        [OpenApiSecurity("function_key", SecuritySchemeType.ApiKey, Name = "code", In = OpenApiSecurityLocationType.Query)]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The OK response")]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.BadRequest, contentType: "text/plain", bodyType: typeof(string), Description = "The 400 response")]
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            string responce = "Random poins API is running OK!";
+
+            string responseMessage = JsonConvert.SerializeObject(responce, Formatting.Indented);
+
+            return new OkObjectResult(responseMessage);
+        }
+    }
+}
+
