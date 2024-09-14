f9afd9c95905363dfd5a992bfccbdd67efd727fe
Martin Georgiev <mggeorgiev@gmail.com>
2024-09-14 10:51:50 +0200

    make functions anonymous

--- points/GenerateRandomPoints.cs
+++ points/GenerateRandomPoints.cs
@@ -31,7 +31,7 @@ namespace points
         [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The OK response")]
         [OpenApiResponseWithBody(statusCode: HttpStatusCode.BadRequest, contentType: "text/plain", bodyType: typeof(string), Description = "The 400 response")]
         public async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req)
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
             _logger.LogInformation(req.ToString());
